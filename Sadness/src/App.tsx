36ba65123dfcd35255185b03521b9af16ffea471
Kevin-kdW <k.r.young192@gmail.com>
2023-06-10 16:16:26 +1200

    Another message?

--- Sadness/src/App.tsx
+++ Sadness/src/App.tsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import RootLayout from "./layouts/RootLayout";
+
+import "./App.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    //<Route path="/" element={<RootLayout />} />
+    <Route path="/" element={RootLayout}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
   )
-)
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <button onClick={() => setCount((count) => count + 1)}>Click {count}</button>
-    </>
-
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
